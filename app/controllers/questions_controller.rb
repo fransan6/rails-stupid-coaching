f2d5a84bd701d4f63b156dc96be3ab78a2119cca
francesca sandford <fran6san@gmail.com>
2023-01-17 20:28:32 +0000

    challenge complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,13 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:answer]
+
+    if @question.downcase == 'i am going to work'
+      @response = 'Great!'
+    elsif @question.end_with?('?')
+      @response = 'Silly question, get dressed and go to work!'
+    else
+      @response = 'I don\'t care, get dressed and go to work!'
+    end
   end
 end
